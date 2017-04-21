29e92103cfcf48c72a7e942d5d94a5ccde25396b
TrinhVanBinh <trinhvanbinh.k1tcu@gmail.com>
2017-04-21 00:27:04 -0700

    lan2

--- dayso.sh
+++ dayso.sh
@@ -1,29 +1,29 @@
 echo "Nhap n"
 read n
-for ((i=0;i<n;i++))
+for ((i=0;i<$n;i++))
 	do
 		echo -n "a[$i]"
 		read a[$i]
 	done
 #in day
-for ((i=0;i<n;i++))
+for ((i=0;i<$n;i++))
 	do
 		echo ${a[$i]}
 		echo ""
 	done
 #tinh tong
 s=0
-for ((i=0;i<n;i++))
+for ((i=0;i<$n;i++))
 	do
 		s=$(($s+${a[$i]}))
 	done
 	echo "Tong la:$s"
-#sap xep
-for ((i=0;i<n-1;i++))
+#sap xep tang dan
+for ((i=0;i<($($n-1));i++))
 	do
 		for((j=$i;j<$n;j++))
 		do
-			if [[${arr[$i]} -gt ${arr[$j]}]]
+			if [[ ${arr[$i]} -gt ${arr[$j]} ]]
 			then
 			
 			t=${arr[$i]}
@@ -32,7 +32,10 @@ for ((i=0;i<n-1;i++))
 			fi
 		done
 	done
+
+
 for ((i=0;i<$n;i++))
 do
-echo "arr[$i]:${arr[$i]}"
+echo -n "${arr[$i]} "
 done
+echo "tb"
